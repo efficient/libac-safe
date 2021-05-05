4f79fe30af915c51404176018d349fd2d12c3619
Sol Boucher <sboucher@cmu.edu>
2021-05-05 16:46:29 -0400

    example.c: Demonstrate inappopriate global variable access
    Debugging *without* LIBGOTCHA_NOGLOBALS= reveals that the wrong copy of stderr is used!

--- example.c
+++ example.c
@@ -8,9 +8,26 @@ static void *realm(void *args) {
 	return NULL;
 }
 
+struct realm2 {
+	int (*fputs)(const char *, FILE *);
+	FILE *stderr;
+};
+
+static void *realm2(void *args) {
+	struct realm2 *io = args;
+	io->fputs("realm2()\n", io->stderr);
+	return NULL;
+}
+
 int main(void) {
 	realm(NULL);
 	isolate(realm, NULL);
+
+	struct realm2 io = {
+		.fputs = fputs,
+		.stderr = stderr,
+	};
+	isolate(realm2, &io);
 
 	return 0;
 }
