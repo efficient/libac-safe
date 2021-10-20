f080347565536203741c7d2005674ec7e4a8a0a8
Sol Boucher <sboucher@cmu.edu>
2021-10-20 15:04:00 -0400

    minimal.rs: Add more minimal example for studying the effect of optimization and LLVM passes

---
+++ minimal.rs
@@ -0,0 +1,20 @@
+mod guarded;
+
+fn main() {
+	use std::panic::catch_unwind;
+
+	assert!(realm());
+	assert!(catch_unwind(panic).is_err());
+}
+
+#[inline(never)]
+fn realm() -> bool {
+	use guarded::Bool;
+
+	*Bool::tagged("Bool")
+}
+
+#[inline(never)]
+fn panic() {
+	panic!();
+}
